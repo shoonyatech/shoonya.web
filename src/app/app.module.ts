a7bcfed664ac68eea83225f0727af4bc9440cc46
Souvik Basu <souvikbasu@gmail.com>
2019-01-18 14:06:12 +0530

    add copyright component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { MatToolbarModule } from "@angular/material/toolbar";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from './footer/footer.component';
+import { CopyrightComponent } from './copyright/copyright.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, CopyrightComponent],
   imports: [BrowserModule, BrowserAnimationsModule, MatToolbarModule],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/copyright/copyright.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "shn-copyright",
+  templateUrl: "./copyright.component.html",
+  styleUrls: ["./copyright.component.scss"]
+})
+export class CopyrightComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
