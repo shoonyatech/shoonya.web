05a4cbf39cf4c6ba0222439f7a0b97fa77cdd8a9
Souvik Basu <souvikbasu@gmail.com>
2019-05-04 22:18:24 +0530

    add Home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,40 @@ import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from "@angular/core";
 import { MatToolbarModule } from "@angular/material/toolbar";
+import { RouterModule, Routes } from "@angular/router";
 
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
-import { FooterComponent } from './footer/footer.component';
-import { CopyrightComponent } from './copyright/copyright.component';
-import { HomeComponent } from './home/home.component';
-import { ContactUsComponent } from './contact-us/contact-us.component';
-import { SectionComponent } from './section/section.component';
+import { FooterComponent } from "./footer/footer.component";
+import { CopyrightComponent } from "./copyright/copyright.component";
+import { HomeComponent } from "./home/home.component";
+import { ContactUsComponent } from "./contact-us/contact-us.component";
+import { SectionComponent } from "./section/section.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+
+const appRoutes: Routes = [
+  { path: "contactus", component: ContactUsComponent },
+  { path: "", component: HomeComponent },
+  { path: "**", component: PageNotFoundComponent }
+];
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, CopyrightComponent, HomeComponent, ContactUsComponent, SectionComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MatToolbarModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CopyrightComponent,
+    HomeComponent,
+    ContactUsComponent,
+    SectionComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    RouterModule.forRoot(appRoutes, { enableTracing: false }),
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
